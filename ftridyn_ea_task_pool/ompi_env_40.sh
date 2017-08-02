bd61d1e877ad04fa62bfe43226ef4cb16f982bb6
Timothy Younkin <tyounkin@vols.utk.edu>
2017-08-02 11:25:51 -0700

    added ftridyn example using task pool for normal node execution and open mpi

---
+++ ftridyn_ea_task_pool/ompi_env_40.sh
@@ -0,0 +1,4 @@
+export OMPI_ROOT=/project/projectdirs/atom/users/elwasif/ompi/install_4.0
+export PATH=$OMPI_ROOT/bin:$PATH
+export LD_LIBRARY_PATH=$OMPI_ROOT/lib64:$LD_LIBRARY_PATH
+export MANPATH=$OMPI_ROOT/share/man:$MANPATH
