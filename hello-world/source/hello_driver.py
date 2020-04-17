e29444c7e2b20b713067f1cc0f1f0f07d24e42cb
elwasif <elwasifwr@ornl.gov>
2020-04-17 13:45:51 -0400

    Update to use Python3

--- hello-world/source/hello_driver.py
+++ hello-world/source/hello_driver.py
@@ -5,20 +5,20 @@ from  component import Component
 class hello_driver(Component):
     def __init__(self, services, config):
         Component.__init__(self, services, config)
-        print 'Created %s' % (self.__class__)
+        print('Created %s' % (self.__class__))
 
     def init(self, timeStamp=0.0):
         return
 
     def step(self, timeStamp=0.0):
-        print 'hello_driver: beginning step call' 
+        print('hello_driver: beginning step call') 
         try:
             worker_comp = self.services.get_port('WORKER')
         except Exception:
             self.services.exception('Error accessing worker component')
             raise
         self.services.call(worker_comp, 'step', 0.0)
-        print 'HelloDriver: finished worker call' 
+        print('HelloDriver: finished worker call') 
         return
 
     def finalize(self, timeStamp=0.0):
--- hello-world/source/hello_worker.py
+++ hello-world/source/hello_worker.py
@@ -5,13 +5,13 @@ from  component import Component
 class hello_worker(Component):
     def __init__(self, services, config):
         Component.__init__(self, services, config)
-        print 'Created %s' % (self.__class__)
+        print('Created %s' % (self.__class__))
 
     def init(self, timeStamp=0.0):
         return
 
     def step(self, timeStamp=0.0):
-        print 'Hello from hello_worker'
+        print('Hello from hello_worker')
         return
     
     def finalize(self, timeStamp=0.0):
