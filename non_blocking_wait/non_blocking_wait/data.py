bcbcda6756ccc2ccffc53c4e6762ce7f59c3a14d
cianciosa <cianciosamr@ornl.gov>
2022-05-12 17:12:35 -0400

    Working version to test out non_blocking data generation.

--- non_blocking_wait/non_blocking_wait/data.py
+++ non_blocking_wait/non_blocking_wait/data.py
@@ -1,12 +1,22 @@
+from ipsframework import Component
 import tensorflow
 import adaptive
+import numpy
+import surrogate
+import time
 
 def function(x):
     return tensorflow.sin(tensorflow.exp(x))
 
+def to_json_compat(database):
+    json_data = {}
+    for key in database:
+        json_data[key] = database[key].numpy().tolist()
+    return json_data
+
 class data(Component):
-    def __init__(self, services, config)
-        Component.__init__(self.services, config)
+    def __init__(self, services, config):
+        Component.__init__(self, services, config)
 
     def init(self, timeStamp=0.0):
         self.services.stage_input_files('model_config.json')
@@ -14,10 +24,12 @@ class data(Component):
         self.train_port = self.services.get_port('TRAIN')
 
     def step(self, timeStamp=0.0):
-        lower_range = config['inputs'][0]['lower_range']
-        upper_range = config['inputs'][0]['upper_range']]
+        lower_range = self.config['inputs'][0]['lower_range']
+        upper_range = self.config['inputs'][0]['upper_range']
 
-        initial_data = adaptive.generate_uniform_data(10,
+        batch_size = 10
+
+        initial_data = adaptive.generate_uniform_data(batch_size,
                                                       min=[lower_range],
                                                       max=[upper_range])
 
@@ -26,39 +38,38 @@ class data(Component):
         initial_data[numpy.argmax(initial_data)] = upper_range
 
         training_data = {
-            'x' : tensorflow.reshape(tensorflow.convert_to_tensor(initial_data, dtype=tensorflow.float32),shape),
+            'x' : tensorflow.reshape(tensorflow.convert_to_tensor(initial_data, dtype=tensorflow.float32), (batch_size,)),
             'y' : None
         }
         training_data['y'] = function(training_data['x'])
 
-        adaptive.save_json('new_data.json', training_data)
-        self.services.update_state()
-
         prediction_size = 1000
 
-        pred_data = {'x': None, 'y': None}
-
-        pred_data['x'] = tensorflow.linspace(training_min_x,
-                                             training_max_x,
-                                             prediction_size)
-        pred_data['y'] = function(pred_data['x'])
-
         for i in range(10):
-            wait = self.services.call_nonblocking(self.train_port)
+            adaptive.save_json('new_data.json', to_json_compat(training_data))
+            self.services.update_state()
+            wait = self.services.call(self.train_port, 'init', timeStamp)
+            wait = self.services.call_nonblocking(self.train_port, 'step', timeStamp)
 
-            while true:
+            while True:
                 try:
-                    self.services.wait_call(wait, 'False')
+                    self.services.wait_call(wait, False)
+                    print('new_data ready')
                     break
                 except:
-                    None
+                    pass
 
-                [new_points, err_est] = surrogate.surrogat_adapt_data(training_data, args['batch_size'])
-                training_data['x'] = tensorflow.concat([training_data['x'],tensorflow.convert_to_tensor(new_points)],0)
-                training_data['y'] = tensorflow.concat([training_data['y'],function(tensorflow.convert_to_tensor(new_points))],0)
+                [new_points, err_est] = surrogate.surrogat_adapt_data(training_data, batch_size)
+                new_points = tensorflow.reshape(tensorflow.convert_to_tensor(new_points), (len(new_points),))
+                training_data['x'] = tensorflow.concat([training_data['x'], new_points], 0)
+                training_data['y'] = tensorflow.concat([training_data['y'], function(tensorflow.convert_to_tensor(new_points))], 0)
 
+                time.sleep(60)
+                print('proxy_data')
+
+            self.services.stage_state()
             new_data = adaptive.load_json('new_data.json')
 
             training_data['x'] = tensorflow.concat([training_data['x'], new_data['x']],0)
-            training_data['y'] = tensorflow.concat([training_data['y'], new_data['y']],0)
-}
+            training_data['y'] = tensorflow.concat([training_data['y'], function(new_data['x'])],0)
+            timeStamp += 1.0
--- non_blocking_wait/setup.py
+++ non_blocking_wait/setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'non_blocking_wait',
-    version = '1.0.0'
+    version = '1.0.0',
+    install_requires=['ipsframework'],
+    packages=find_packages(),
 )
